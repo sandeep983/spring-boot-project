21053980f6e611898996f98e1aba79220cc89c58
sandeep983 <sandeeppatel983@hotmail.com>
2023-09-21 23:48:42 +0530

    project created

---
+++ techstarworld/src/main/java/com/techstarworld/TechstarworldApplication.java
@@ -0,0 +1,13 @@
+package com.techstarworld;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechstarworldApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TechstarworldApplication.class, args);
+	}
+
+}
---
+++ techstarworld/src/test/java/com/techstarworld/TechstarworldApplicationTests.java
@@ -0,0 +1,13 @@
+package com.techstarworld;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechstarworldApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
